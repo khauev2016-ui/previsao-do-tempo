8d1f197669e44527c216102aa8e08a6b2784562d
Khaue Vieira <khauev2016@gmail.com>
2026-05-01 16:42:48 -0300

    Projeto previsão do tempo

---
+++ Previsao_Tempo/script.js
@@ -0,0 +1,34 @@
+const API_KEY = "b63a31c92b40f3ae0828edb83e847430"; // coloque sua chave aqui, site usado: OpenWeather
+
+async function buscarClima() {
+  const cidade = document.getElementById("cidade").value;
+
+  if (!cidade) {
+    alert("Digite uma cidade!");
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade},BR&appid=${API_KEY}&units=metric&lang=pt_br`;
+
+  try {
+    const response = await fetch(url);
+    const dados = await response.json();
+
+    if (dados.cod !== 200) {
+      document.getElementById("resultado").innerHTML = "Cidade não encontrada!";
+      return;
+    }
+
+    document.getElementById("resultado").innerHTML = `
+      <h2>${dados.name}</h2>
+      <p>🌡️ Temperatura: ${dados.main.temp} °C</p>
+      <p>☁️ Clima: ${dados.weather[0].description}</p>
+      <p>💧 Umidade: ${dados.main.humidity}%</p>
+      <p>💨 Vento: ${dados.wind.speed} m/s</p>
+    `;
+    
+  } catch (erro) {
+    document.getElementById("resultado").innerHTML = "Erro ao buscar dados!";
+    console.error(erro);
+  }
+}
